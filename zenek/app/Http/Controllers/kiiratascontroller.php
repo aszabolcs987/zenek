3cd63fe1d6595a5594c0b1afe42cfb779251cf67
aszabolcs987 <91121143+aszabolcs987@users.noreply.github.com>
2024-10-16 22:08:34 +0200

    Update kiiratascontroller.php

--- zenek/app/Http/Controllers/kiiratascontroller.php
+++ zenek/app/Http/Controllers/kiiratascontroller.php
@@ -7,13 +7,12 @@ use Illuminate\Http\Request;
 
 class kiiratascontroller extends Controller
 {
-    // létrehozok egy függvényt
     public function getContacts(Request $request)
     {
-        // Minden adatot lekér a zenek táblából
+        // leker minden adatot a  tablabol
         $zenek = zenek::all();
 
-        // átadja az adatokat egy $zenek változóba a view-nak
+        // atadja ezeket a success viewnek $zenek valtozoba
         return view('success', ['zenek' => $zenek]);
     }
 }
