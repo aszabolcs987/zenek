64ae851059e27c63d23c001a95554fe154fdfab2
aszabolcs987 <91121143+aszabolcs987@users.noreply.github.com>
2024-10-16 22:05:00 +0200

    Update zenek.php

--- zenek/app/Models/zenek.php
+++ zenek/app/Models/zenek.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Models;
-// php artisan make:model zenek
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class zenek extends Model
 {
+    // php artisan make:model zenek
     use HasFactory;
-    // megadom, hogy ne adjon hozzá pluszba módosítási és hozzá adási időt a táblához
+    // ne adjon hozza pluszba modositasi es mentesi idpot
     public $timestamps = false;
     // és, hogy a tábla neve zenek lesz
     protected $table = 'zenek';
